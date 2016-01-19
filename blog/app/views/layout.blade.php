fd3bf090b7474d31c4b9b4470a6225f954413c06
Grant Spence <gcs278@vt.edu>
2016-01-18 22:14:49 -0500

    Google Analytics

--- blog/app/views/layout.blade.php
+++ blog/app/views/layout.blade.php
@@ -47,6 +47,17 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <META NAME="ROBOTS" CONTENT="INDEX, FOLLOW">
 
+    <!--Google Analytics-->
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-72603402-1', 'auto');
+      ga('send', 'pageview');
+    </script>
+
 </head>
 @if( Request::is('/') )
 <body id="main" class="home">
@@ -116,7 +127,7 @@
                     </div>
                 </div>
             </nav>
-            
+
             @yield('content')
             </div>
                 <div class="clearfooter"></div>
