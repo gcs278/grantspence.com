8a336debac0f9b616a20bf0ba4f5ad804bf5b619
Grant Spence <gcs278@vt.edu>
2016-01-18 22:02:11 -0500

    Fixed autoformat issue

--- blog/app/views/development.blade.php
+++ blog/app/views/development.blade.php
@@ -30,19 +30,24 @@ DEVELOPMENT
                 <div class="dev_intro clearfix">
                     <h1 class="proj-strikethrough">{{$names[$key]}}</h1>
 
-                    @foreach ($project as $post)
+                    @foreach ($project as $page)
+                    <?php
+                        global $post;
+                        $post = get_post($page->ID);
+                        setup_postdata( $post );
+                    ?>
                     <div class="row project">
                         <div class="hidden-sm hidden-xs col-md-4 col-md-offset-0 col-sm-8 col-sm-offset-2">
-                            <?php $thumb_id = get_post_thumbnail_id($post->ID);
+                            <?php $thumb_id = get_post_thumbnail_id($page->ID);
                                 $thumb_url = wp_get_attachment_image_src($thumb_id,'thumbnail-size', true)[0];
                             ?>
                             <img src="{{$thumb_url}}">
                         </div>
-                        <?php $custom = get_post_custom($post->ID);
+                        <?php $custom = get_post_custom($page->ID);
                         ?>
                         <div class="col-md-7 col-sm-12 project_details">
                             <h2>
-                                {{$post->post_title}}
+                                {{$page->post_title}}
                             </h2>
                             @if (array_key_exists('url',$custom))
                                 @if ( preg_match("/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/", $custom['url'][0]) && strcmp($custom['url'][0],"halyourrealestatepal.com"))
@@ -56,15 +61,16 @@ DEVELOPMENT
                             @endif
                             <div class="row hidden-lg hidden-md ">
                                 <div class="col-sm-12 col-sm-offset-0">
-                                    <?php $thumb_id = get_post_thumbnail_id($post->ID);
+                                    <?php $thumb_id = get_post_thumbnail_id($page->ID);
                                         $thumb_url = wp_get_attachment_image_src($thumb_id,'thumbnail-size', true)[0];
                                     ?>
                                     <img src="{{$thumb_url}}">
                                 </div>
                             </div>
-                            <div class="proj-desc">{{do_shortcode($post->post_content)}}</div>
+                            <div class="proj-desc">{{the_content()}}</div>
                         </div>
                     </div>
+                    <?php wp_reset_postdata( $post );?>
                     @endforeach
                 </div>
             </div>
